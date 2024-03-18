521066066e8a4e7fd82f9d9ac11a079424cd82ad
Luiz Guilherme de Souza Gonçalves <103260099+Luizgi@users.noreply.github.com>
2024-03-18 11:58:07 -0300

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [Header("Car Settings")]
+    public float driftFactor = 0.95f;
+    public float accelarationFactor = 30.0f;
+    public float turnFactor = 1.5f;
+    public float maxSpeed = 20;
+
+    [Header("Local")]
+    [SerializeField] float accelerationInput = 0;
+    [SerializeField] float steeringInput = 0;
+    [SerializeField] float rotationAngle = 0;
+    [SerializeField] float velocityVsUp = 0;
+
+    Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        ApplyEngineForce();
+
+        KillOrthogonalVelocity();
+
+        ApplySteering();
+    }
+
+
+
+    private void ApplyEngineForce()
+    {
+        velocityVsUp = Vector2.Dot(transform.up, rb2d.velocity);
+        if (velocityVsUp > maxSpeed && accelerationInput > 0)
+            return;
+        if(velocityVsUp < -maxSpeed * 0.5f && accelerationInput < 0) 
+            return;
+        if (rb2d.velocity.sqrMagnitude > maxSpeed * maxSpeed && accelerationInput > 0)
+            return;
+
+
+        if (accelerationInput == 0)
+            rb2d.drag = Mathf.Lerp(rb2d.drag, 3.0f, Time.fixedDeltaTime * 3);
+        else 
+            rb2d.drag = 0;
+
+        Vector2 engineForceVector = transform.up * accelerationInput * accelarationFactor;
+        rb2d.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+
+    private void ApplySteering()
+    {
+        float minSpeedBeforeAllowTurningFactor = (rb2d.velocity.magnitude / 8);
+        minSpeedBeforeAllowTurningFactor = Mathf.Clamp01(minSpeedBeforeAllowTurningFactor);
+
+        rotationAngle -= steeringInput * turnFactor * minSpeedBeforeAllowTurningFactor;
+        rb2d.MoveRotation(rotationAngle);
+    }
+
+    void KillOrthogonalVelocity()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rb2d.velocity, transform.up);
+        Vector2 rightVelocity = transform.right * Vector2.Dot(rb2d.velocity, transform.right);
+
+        rb2d.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+
+    public void SetInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    CarController carController;
+
+    private void Awake()
+    {
+        carController = GetComponent<CarController>();
+    }
+
+    private void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+        carController.SetInputVector(inputVector);
+    }
+}
